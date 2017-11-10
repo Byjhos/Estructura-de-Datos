230de3a0af34e0f51bce55333d4dce0329093faf
José Luis <33426794+luisotak@users.noreply.github.com>
2017-11-10 16:28:26 -0400

    Add files via upload

---
+++ subconjuntos.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+public class subconjuntos {
+
+    public static void main(String[] args) {
+        boolean res=true;
+          String a= "abc";
+          String b= "xadfbhdc";
+        	for (int i = 0; i<a.length(); i++)
+        	{
+        		char d =a.charAt(i);
+        		if(!b.contains(String.valueOf(d)))
+        		{
+        			res=false;
+        			break;
+        		}
+        	}
+        	if(res)
+        	{
+        		System.out.print("La cadena b si es un subconjunto de la cadena a");
+        	}
+        	else
+        		System.out.print("La cadena b no es un subconjunto de la cadena a");
+         }
+}
